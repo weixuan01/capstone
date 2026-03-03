9d45d35bd1050a50b953c81ce0806b27c8ccb135
Poon Wei Xuan <poonweixuan@gmail.com>
2026-03-03 15:42:42 +0800

    Reorganize project hierarchy
    Move scripts folder from capstone/simulation/ to capstone/

---
+++ scripts/build-project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BASE_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")") # get dir of script
+cd $BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/
+source /opt/ros/humble/setup.bash
+colcon build --cmake-args -DBUILD_TESTING=ON -DPython3_EXECUTABLE=/usr/bin/python3
--- scripts/launchsim.sh
+++ scripts/launchsim.sh
@@ -3,8 +3,8 @@
 SIM_TYPE=$1
 
 BASE_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")") # get dir of script
-source $BASE_DIR/../crazyflie_mapping_demo/ros2_ws/install/setup.bash # assumes crazyflie_mapping_demo and scripts folder are always in the same dir (simulation)
-export GZ_SIM_RESOURCE_PATH="$BASE_DIR/../crazyflie_mapping_demo/simulation_ws/crazyflie-simulation/simulator_files/gazebo/"
+source $BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/install/setup.bash # assumes scripts and simulation folder are always in the same dir (capstone)
+export GZ_SIM_RESOURCE_PATH="$BASE_DIR/../simulation/crazyflie_mapping_demo/simulation_ws/crazyflie-simulation/simulator_files/gazebo/"
 
 if [ $SIM_TYPE = "wallfollowing" ]
 then
