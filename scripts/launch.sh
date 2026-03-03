de7e977e3442ecce0b42ba93c4c82d946c5f4dde
Poon Wei Xuan <poonweixuan@gmail.com>
2026-03-03 16:05:21 +0800

    Add real mapping/wallfollowing to launch script
    Rename launchsim.sh to launch.sh

---
+++ scripts/launch.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+MODE=$1
+TYPE=$2
+
+BASE_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")") # get dir of script
+source $BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/install/setup.bash # assumes scripts and simulation folder are always in the same dir (capstone)
+export GZ_SIM_RESOURCE_PATH="$BASE_DIR/../simulation/crazyflie_mapping_demo/simulation_ws/crazyflie-simulation/simulator_files/gazebo/"
+
+
+display_help() {
+    echo "Usage format: ./launch.sh <A> <B>"
+    echo "    A: sim | real"
+    echo "    B: wallfollowing | manual (control using teleop)"
+    exit 1
+}
+
+if [[ $MODE == "sim" ]] 
+then   
+    if [[ $TYPE == "wallfollowing" ]] 
+    then
+	ros2 launch crazyflie_ros2_multiranger_bringup wall_follower_mapper_simulation.launch.py
+    elif [[ $TYPE == "manual" ]] 
+    then
+	ros2 launch crazyflie_ros2_multiranger_bringup simple_mapper_simulation.launch.py &
+        gnome-terminal -- bash -c "ros2 run teleop_twist_keyboard teleop_twist_keyboard; bash"
+    else
+        display_help
+    fi
+
+elif [[ $MODE == "real" ]] 
+then
+    if [[ $TYPE == "wallfollowing" ]] 
+    then
+        ros2 launch crazyflie_ros2_multiranger_bringup wall_follower_mapper_real.launch.py
+    elif [[ $TYPE == "manual" ]] 
+    then
+        ros2 launch crazyflie_ros2_multiranger_bringup simple_mapper_real.launch.py &
+        gnome-terminal -- bash -c "ros2 run teleop_twist_keyboard teleop_twist_keyboard; bash"
+    else
+        display_help
+    fi
+    
+else
+    display_help
+fi
+        
