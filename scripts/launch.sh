0ab72ad92e6e47c0e25aa7f7154e7fa555789f38
Poon Wei Xuan <poonweixuan@gmail.com>
2026-03-10 21:52:20 +0800

    Update launch script to accept a map name arg
    Also removed redundant circle.sdf/stl

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,21 +2,50 @@
 
 MODE=$1
 TYPE=$2
+MAP=$3
 
-BASE_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")") # get dir of script
-source $BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/install/setup.bash # assumes scripts and simulation folder are always in the same dir (capstone)
-export GZ_SIM_RESOURCE_PATH="$BASE_DIR/../simulation/crazyflie_mapping_demo/simulation_ws/crazyflie-simulation/simulator_files/gazebo/"
-
+BASE_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")") # Get dir of script
+SIM_LAUNCH_PATH="$BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/src/ros_gz_crazyflie/ros_gz_crazyflie_bringup/launch/crazyflie_simulation.launch.py"
 
 display_help() {
-    echo "Usage format: ./launch.sh <A> <B>"
-    echo "    A: sim | real"
-    echo "    B: wallfollowing | manual | square | frontier-exploration"
-    exit 1
+    echo "Usage format: ./launch.sh <MODE> <TYPE> [MAP]"
+    echo "    MODE: sim | real"
+    echo "    TYPE: wallfollowing | manual | square | frontier-exploration"
+    echo "    MAP: (Optional, for sim only) crazyflie_world | custom | maze | circle-maze"
+    exit 1 # Terminates the script
 }
 
+source $BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/install/setup.bash # Assumes scripts and simulation folder are always in the same dir (capstone)
+export GZ_SIM_RESOURCE_PATH="$BASE_DIR/../simulation/crazyflie_mapping_demo/simulation_ws/crazyflie-simulation/simulator_files/gazebo/"
+
+if [[ -z "$MODE" ]] || [[ -z "$TYPE" ]] # If no. of input parameters < 2
+then
+    display_help
+fi
+
 if [[ $MODE == "sim" ]] 
-then   
+then
+    # If type is not valid
+    if [[ "$TYPE" != "wallfollowing" && "$TYPE" != "manual" && "$TYPE" != "square" && "$TYPE" != "frontier-exploration" ]]
+    then
+        display_help
+    fi
+
+    # Check if map parameter is not empty
+    if [[ -n "$MAP" ]]
+    then
+        # Check if the input map name is not the current map, otherwise skip overwrite
+        if ! grep -q "$MAP.sdf -r" "$SIM_LAUNCH_PATH"
+        then
+            # sed -i 's/pattern/replacement/'
+            sed -i "s/[^ ']*\.sdf -r/$MAP.sdf -r/" "$SIM_LAUNCH_PATH"
+
+            # Rebuild project
+            "$BASE_DIR/build-project.sh"
+            source $BASE_DIR/../simulation/crazyflie_mapping_demo/ros2_ws/install/setup.bash # Re-source after rebuilding
+        fi
+    fi
+
     if [[ $TYPE == "wallfollowing" ]] 
     then
 	    ros2 launch crazyflie_ros2_multiranger_bringup wall_follower_mapper_simulation.launch.py
@@ -30,8 +59,6 @@ then
     elif [[ $TYPE == 'frontier-exploration' ]]
     then
         ros2 launch crazyflie_ros2_multiranger_bringup frontier_exploration_mapper_simulation.launch.py
-    else
-        display_help
     fi
 
 elif [[ $MODE == "real" ]] 
